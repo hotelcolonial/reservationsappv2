b327e5e45da52a4eff9b670202844fb640ddad1b
edmblue <brsarayb@gmail.com>
2025-10-07 11:32:10 -0300

    metada fixed

--- app/admin/(dashboard)/layout.tsx
+++ app/admin/(dashboard)/layout.tsx
@@ -2,8 +2,32 @@ import AppSidebar from "@/components/AppSidebar";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { Metadata } from "next";
 import { cookies } from "next/headers";
 import { Toaster } from "sonner";
+
+export const metadata: Metadata = {
+  title: "Dashboard Geral | Admin Hotel Colonial Iguaçu",
+  description:
+    "Painel de administração para visualizar o resumo geral das reservas dos eventos especiais de fim de ano.",
+
+  // ¡MUY IMPORTANTE! Esto evita que Google y otros buscadores indexen tu panel de admin.
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+    },
+  },
+
+  // Opcional: Añade un favicon para una apariencia más profesional
+  icons: {
+    icon: "/favicon.ico", // Asegúrate de tener un favicon.ico en tu carpeta /public
+  },
+};
 
 export default async function AdminLayout({
   children,
